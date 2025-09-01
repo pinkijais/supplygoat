c82d79c5ba492822438aa1143c8ccab50bb9d2b2
pinkijais <98391838+pinkijais@users.noreply.github.com>
2025-09-01 15:50:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket" "data" {
   tags = {
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
-    access_key = "AKIAIOSFODNN7EXAMPLF
+  
   }
 }
 
